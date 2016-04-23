0a6634d4f2f590326a0cdeb52d6869a2636090db
xvasko14 <vaskomichal7@gmail.com>
2016-04-23 13:17:26 +0200

    Added files via upload

---
+++ ClassLibrary1/ClassLibrary1/Class1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        public static double Function(double num1, double num2, string op)
+        {
+            double finRes = 0;
+            if (op == "plus")
+            {
+                finRes = num1 + num2;
+            }
+            else if (op == "minus")
+            {
+                finRes = num1 - num2;
+            }
+            else if (op == "multiple")
+            {
+                finRes = num1 * num2;
+            }
+            else if (op == "divide")
+            {
+                finRes = num1 / num2;
+            }
+            else if (op == "exp")
+            {
+                finRes = Math.Pow(num1, num2);
+            }
+            else if (op == "fac")
+            {
+                double result = num1;
+                for (double i = (num1) - 1; i > 0; i--)
+                {
+                    result = result * i;
+                    finRes = result;
+                }
+            }
+            else if (op == "sqrt")
+            {
+                finRes = Math.Sqrt(num1);
+            }
+            return finRes;
+        }
+    }
+}
